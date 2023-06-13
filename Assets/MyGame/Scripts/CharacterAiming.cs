1917096e0ab4d7b5b7495d2b08e59b91a33e5c8a
Thuc Tran <thuc.ttk@gmail.com>
2023-06-13 19:16:58 +0700

    [add] player aiming weapon

--- Assets/MyGame/Scripts/CharacterAiming.cs
+++ Assets/MyGame/Scripts/CharacterAiming.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class CharacterAiming : MonoBehaviour
 {
     public float turnSpeed = 15;
+    public float aimDuration = 0.3f;
+    public Rig aimLayer;
+
     private Camera mainCamera;
 
     private void Awake()
@@ -16,6 +20,18 @@ public class CharacterAiming : MonoBehaviour
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            aimLayer.weight += Time.deltaTime / aimDuration;
+        }
+        else
+        {
+            aimLayer.weight -= Time.deltaTime / aimDuration;
+        }
     }
 
     void FixedUpdate()
