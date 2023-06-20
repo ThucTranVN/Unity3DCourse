eae9905d529babf7a7636b12dece8033aef7192d
Thuc Tran <thuc.ttk@gmail.com>
2023-06-20 19:13:59 +0700

    [add] player pickup weapon

---
+++ Assets/MyGame/Scripts/GamePlay/ActiveWeapon.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveWeapon : MonoBehaviour
+{
+    public Transform crosshairTarget;
+    public Transform weaponParent;
+    public Animator rigController;
+    private RaycastWeapon raycastWeapon;
+
+    private void Awake()
+    {
+        RaycastWeapon existWeapon = GetComponentInChildren<RaycastWeapon>();
+        if (existWeapon)
+        {
+            Equip(existWeapon);
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (raycastWeapon)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                raycastWeapon.StartFiring();
+            }
+
+            if (raycastWeapon.isFiring)
+            {
+                raycastWeapon.UpdateFiring(Time.deltaTime);
+            }
+
+            raycastWeapon.UpdateBullets(Time.deltaTime);
+
+            if (Input.GetButtonUp("Fire1"))
+            {
+                raycastWeapon.StopFiring();
+            }
+
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                bool isHolstered = rigController.GetBool("holster_weapon");
+                rigController.SetBool("holster_weapon", !isHolstered);
+            }
+        }
+    }
+
+    public void Equip(RaycastWeapon newWeapon)
+    {
+        if (raycastWeapon)
+        {
+            Destroy(raycastWeapon.gameObject);
+        }
+        raycastWeapon = newWeapon;
+        raycastWeapon.raycastDestination = crosshairTarget;
+        raycastWeapon.transform.parent = weaponParent;
+        raycastWeapon.transform.localPosition = Vector3.zero;
+        raycastWeapon.transform.localRotation = Quaternion.identity;
+        rigController.Play("equip_" + raycastWeapon.weaponName);
+    }
+}
--- Assets/MyGame/Scripts/GamePlay/CharacterAiming.cs
+++ Assets/MyGame/Scripts/GamePlay/CharacterAiming.cs
@@ -1,21 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Animations.Rigging;
 
 public class CharacterAiming : MonoBehaviour
 {
     public float turnSpeed = 15;
-    public float aimDuration = 0.3f;
-    public Rig aimLayer;
+    public Transform ignoreZone;
+    public float radius;
+    public bool showGizmos;
 
     private Camera mainCamera;
-    private RaycastWeapon raycastWeapon;
 
     private void Awake()
     {
         mainCamera = Camera.main;
-        raycastWeapon = GetComponentInChildren<RaycastWeapon>();
     }
 
     void Start()
@@ -24,42 +22,25 @@ public class CharacterAiming : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    private void Update()
-    {
-        if (aimLayer)
-        {
-            if (Input.GetButton("Fire2"))
-            {
-                aimLayer.weight += Time.deltaTime / aimDuration;
-            }
-            else
-            {
-                aimLayer.weight -= Time.deltaTime / aimDuration;
-            }
-        }
-       
-
-        if (Input.GetButtonDown("Fire1"))
-        {
-            raycastWeapon.StartFiring();
-        }
-
-        if (raycastWeapon.isFiring)
-        {
-            raycastWeapon.UpdateFiring(Time.deltaTime);
-        }
-        
-        raycastWeapon.UpdateBullets(Time.deltaTime);
-
-        if (Input.GetButtonUp("Fire1"))
-        {
-            raycastWeapon.StopFiring();
-        }
-    }
-
     void FixedUpdate()
     {
         float yawCamera = mainCamera.transform.rotation.eulerAngles.y;
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), turnSpeed * Time.fixedDeltaTime);
+
+        Collider[] colliders = Physics.OverlapSphere(ignoreZone.position, radius);
+        foreach (var collider in colliders)
+        {
+            if (!collider.gameObject.layer.Equals(LayerMask.NameToLayer("Ignore Raycast")))
+            {
+                collider.gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!showGizmos) return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(ignoreZone.position, radius);
     }
 }
